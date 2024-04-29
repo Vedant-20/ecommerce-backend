dc684cdb20ca66c92539b117339f3f6f9a9de3c5
Vedant <vedantwarjurkar@gmail.com>
2024-04-29 10:00:52 +0530

    cron jobs started in index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
-import { app } from './app.js';
+import { app } from "./app.js";
+import job from "./cron/cron.js";
 
 dotenv.config({
-    path:'./.env'
-})
+  path: "./.env",
+});
+
+job.start();
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000 , ()=>{
-        console.log(`Server is running at port : ${process.env.PORT}`)
-    })
-})
-.catch((err)=>{
-    console.log("MongoDb connection failed !!!!",err)
-})
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDb connection failed !!!!", err);
+  });
